9a2b06a9f10e6b5a4c43b9d1d5aba069e652fc68
BillyPittman1993 <billytrading93@gmail.com>
2019-05-13 15:43:49 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,9 +4,11 @@ class Anagram
   attr_accessor :word
   
   @@anagrams = []
+  @@words = []
   
   def initialize(word)
     @word = word 
+    @@anagrams << word 
   end
   
   def word 
@@ -14,7 +16,7 @@ class Anagram
   end
   
   def self.match(word)
-    
+    @@anagrams.each do |word| 
   end
   
 end 
