f71c13d4e652e08c51e69cc9ea67b29e96f75068
BillyPittman1993 <billytrading93@gmail.com>
2019-05-13 16:08:33 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,19 +3,11 @@ class Anagram
   
   attr_accessor :word
   
-  @@anagrams = ["enlists google inlets banana"]
-  @@words = []
-  
   def initialize(word)
     @word = word 
-    @@anagrams << word 
   end
   
-  def word 
-    @word
-  end
-  
-  def self.match(word)
+  def match(word)
     
   end
   
